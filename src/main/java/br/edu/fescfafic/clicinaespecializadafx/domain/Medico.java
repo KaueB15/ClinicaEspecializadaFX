f18dc4ea61dd5f6d88a8cf346709327e100114ce
Junior <jijunior1026@gmail.com>
2024-05-18 20:22:31 -0300

    Inserções de métodos nos daos Paciente e Medico

--- src/main/java/br/edu/fescfafic/clicinaespecializadafx/domain/Medico.java
+++ src/main/java/br/edu/fescfafic/clicinaespecializadafx/domain/Medico.java
@@ -5,6 +5,10 @@ import lombok.Data;
 
 @Entity
 @Data
+@NamedQueries({
+        @NamedQuery(name = "medicos.getAll", query = "select m from Medico m"),
+        @NamedQuery(name = "medicos.getByCrm", query = "select m from Medico m where m.crm=:crm"),
+        @NamedQuery(name = "medicos.getByEspecialidade", query = "select m from Medico m where m.especialidade=:especialidade")})
 public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,7 +16,7 @@ public class Medico {
     @Column(unique = true)
     private int crm;
     private String nome, especialidade;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     private Login login;
     @Column(unique = true)
     private String cpf;
--- src/main/java/br/edu/fescfafic/clicinaespecializadafx/domain/Paciente.java
+++ src/main/java/br/edu/fescfafic/clicinaespecializadafx/domain/Paciente.java
@@ -5,12 +5,16 @@ import lombok.Data;
 
 @Entity
 @Data
+@NamedQueries({
+        @NamedQuery(name = "pacientes.getAll", query = "select p from Paciente p"),
+        @NamedQuery(name = "pacientes.getByCpf", query = "select p from Paciente p where p.cpf=:cpf")
+})
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     private Login login;
     @Column(unique = true)
     private String cpf;
