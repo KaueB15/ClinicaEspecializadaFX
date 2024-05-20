dfe4e6649d0bd54bdb318416562162f004304a35
Kaue Batista <kauecardozo3@gmail.com>
2024-05-20 14:04:37 -0300

    Login Controller

---
+++ src/main/java/br/edu/fescfafic/clicinaespecializadafx/main/App.java
@@ -0,0 +1,11 @@
+package br.edu.fescfafic.clicinaespecializadafx.main;
+
+import br.edu.fescfafic.clicinaespecializadafx.dao.SendSMSDAO;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+
+}
