6e3beec4426d82bc531102354bdf99499aa6e05c
Lukaspersy <lukaspersy@yahoo.com>
2024-05-28 13:17:25 -0300

    Correção passwordField cadastros

--- src/main/java/br/edu/fescfafic/clicinaespecializadafx/view/CadastroMedicoController.java
+++ src/main/java/br/edu/fescfafic/clicinaespecializadafx/view/CadastroMedicoController.java
@@ -12,10 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.hibernate.exception.ConstraintViolationException;
 
@@ -30,7 +27,7 @@ import java.time.LocalDate;
         @FXML
         private TextField fieldLogin;
         @FXML
-        private TextField fieldPassword;
+        private PasswordField fieldPassword;
         @FXML
         private TextField fieldEspecialidade;
         @FXML
